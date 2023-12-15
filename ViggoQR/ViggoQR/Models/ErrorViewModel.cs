9b10ad28af132a0c83df6b3c3060c28934c9a8f7
Joel <j.p.santander@outlook.com>
2023-12-15 12:42:05 -0300

    Creando Página

---
+++ ViggoQR/ViggoQR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViggoQR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
