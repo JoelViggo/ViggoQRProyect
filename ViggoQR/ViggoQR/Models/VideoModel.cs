7109779c47c3e48cfed1135d6a7c85b79ae0b247
Joel <j.p.santander@outlook.com>
2023-12-17 23:16:24 -0300

    Actualización de avance

---
+++ ViggoQR/ViggoQR/Models/VideoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ViggoQR.Models
+{
+    public class VideoModel
+    {
+        public string Title { get; set; }
+        public string FilePath { get; set; }
+        public string Description { get; set; }
+    }
+}
